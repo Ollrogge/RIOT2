1effd68a0db5875bb2457f3434dd5a5b3455d69f
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2021-02-10 17:39:08 +0200

    tests: Add minimal psa_crypto test app

---
+++ tests/psa_crypto/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "psa/crypto.h"
+
+int main(void)
+{
+    if (psa_crypto_init() == PSA_SUCCESS) {
+        puts("PSA Init successful");
+    }
+
+    psa_hash_operation_t sha1 = psa_hash_operation_init();
+    psa_hash_operation_t sha256 = psa_hash_operation_init();
+
+    int status = psa_hash_setup(&sha1, PSA_ALG_SHA_1);
+    if (status != PSA_SUCCESS) {
+        printf("SHA1 Setup failed: %d\n", status);
+    }
+
+    status = psa_hash_setup(&sha256, PSA_ALG_SHA_256);
+    if (status != PSA_SUCCESS) {
+        printf("SHA256 Setup failed: %d\n", status);
+    }
+
+    return 1;
+}
