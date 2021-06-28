b867623939fc8b5cb1e8faf598adb01f504e2fd2
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2021-06-28 18:03:55 +0200

    examples: Add example application for PSA crypto

---
+++ examples/psa-crypto-example/main.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Hello World application
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include "psa/crypto.h"
+
+static uint8_t KEY[] = {
+    0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+    0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+};
+static uint8_t KEY_LEN = 16;
+
+static uint8_t __attribute__((aligned)) ECB_PLAIN[] = {
+    0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96,
+    0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
+    0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96,
+    0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
+};
+static uint8_t ECB_PLAIN_LEN = 32;
+
+static uint8_t __attribute__((aligned))ECB_CIPHER[] = {
+    0x3a, 0xd7, 0x7b, 0xb4, 0x0d, 0x7a, 0x36, 0x60,
+    0xa8, 0x9e, 0xca, 0xf3, 0x24, 0x66, 0xef, 0x97,
+    0x3a, 0xd7, 0x7b, 0xb4, 0x0d, 0x7a, 0x36, 0x60,
+    0xa8, 0x9e, 0xca, 0xf3, 0x24, 0x66, 0xef, 0x97
+};
+static uint8_t ECB_CIPHER_LEN = 32;
+
+int main(void)
+{
+    psa_status_t status = PSA_ERROR_DOES_NOT_EXIST;
+
+    psa_key_attributes_t attr = psa_key_attributes_init();
+    psa_key_lifetime_t ext_lifetime = 0x00000100;
+    psa_key_lifetime_t int_lifetime = 0;
+    psa_key_id_t key_id = 0;
+
+    uint8_t cipher_out[ECB_CIPHER_LEN];
+    size_t output_len = 0;
+
+    psa_set_key_lifetime(&attr, int_lifetime);
+    psa_set_key_algorithm(&attr, PSA_ALG_ECB_NO_PADDING);
+    psa_set_key_bits(&attr, 128);
+    psa_set_key_type(&attr, PSA_KEY_TYPE_AES);
+
+    status = psa_import_key(&attr, KEY, KEY_LEN, &key_id);
+    if (status != PSA_SUCCESS) {
+        printf("Import failed: %d\n", status);
+        return -1;
+    }
+
+    status = psa_cipher_encrypt(key_id, PSA_ALG_ECB_NO_PADDING, ECB_PLAIN, ECB_PLAIN_LEN, cipher_out, ECB_CIPHER_LEN, &output_len);
+    if (status != PSA_SUCCESS) {
+        printf("Encrypt failed: %d\n", status);
+        return -1;
+    }
+
+    if (memcmp(cipher_out, ECB_CIPHER, ECB_CIPHER_LEN)) {
+        puts("Encryption failed");
+    }
+    else {
+        puts("Encryption successful");
+    }
+
+    return 0;
+}
